6b1e0d81de3bbaa3f76ab9f9f048955f56de4f6d
gustavohdev <g_a93@outlook.com>
2021-09-14 00:05:54 -0300

    added cards, axios, working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from "./components/Form"
+import React, { useState } from 'react'
+import CardList from "./components/CardList"
 
 function App() {
+
+  const [cards, setCards] = useState([])
+
+  const addNewCard = (card) => {
+    setCards([...cards, card])
+  }
+
   return (
     <div className="App">
       <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
         Search a GitHub User
       </h1>
-      <Form />
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,39 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import axios from "axios";
 
 const Form = (props) => {
-
-    const [username, setUsername] = useState("")
+    const [username, setUsername] = useState("");
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        alert(username)
-    }
+        event.preventDefault();
 
-  return (
-    <form className="w-full max-w-sm mx-auto" onSubmit={handleSubmit}>
-      <div className="mt-10 flex">
-        <input
-          type="text"
-          placeholder="GitHub username"
-          value={username}
-          onChange={ (event) => setUsername(event.target.value)}
-          className="form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150"
-          required
-        />
-        <button 
-          type="submit"
-          className="ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-  )
-}
+        axios.get(`https://api.github.com/users/${username}`).then((resp) => {
+            console.log(resp.data);
+            props.onSubmit(resp.data)
+            setUsername("");
+        });
+    };
 
-export default Form
+    return (
+        <form className="w-full max-w-sm mx-auto" onSubmit={handleSubmit}>
+            <div className="mt-10 flex">
+                <input
+                    type="text"
+                    placeholder="GitHub username"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    className="form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150"
+                    required
+                />
+                <button
+                    type="submit"
+                    className="ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+                >
+                    Search
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default Form;
